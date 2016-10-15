708929c5444763b0faca4f7059140f949c67abfe
Ouwen Huang <oh11@Ouwens-MacBook-Pro.local>
2016-10-14 22:24:07 -0400

    Allow for local backing of docker dbs

---
+++ database/data/README.md
@@ -0,0 +1,8 @@
+Data lives here.
+
+Suggested data structure
+ - test
+For running unit tests
+
+ - dev
+For running app locally
--- database/run.sh
+++ database/run.sh
@@ -5,11 +5,15 @@ if [[ "$(docker images -q postgres 2> /dev/null)" == "" ]]; then
 fi
 
 if [[ "$(docker ps | grep test-postgres 2> /dev/null)" == "" ]]; then
-  docker run -p 5433:5432 --name test-postgres -e POSTGRES_PASSWORD=testtest -d postgres
+  docker run -d -p 5433:5432 \
+    --name test-postgres \
+    -e POSTGRES_PASSWORD=testtest \
+    -v $1/data/test:/var/lib/postgresql/data \
+   postgres
   sleep 3
 fi
 
 docker run -it --rm \
-  -v $1:/src/schema \
+  -v $1/schema:/src/schema \
   --link test-postgres:postgres postgres \
   psql -h postgres -U postgres -v schema_directory="/src/schema" -a -f /src/schema/main.sql
