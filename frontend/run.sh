db980e95ee9dcb72a05d6429b562d3b0b1c79185
Ouwen Huang <oh11@Ouwens-MacBook-Pro.local>
2016-10-14 21:46:30 -0400

    Fixed e2e drivers

--- frontend/run.sh
+++ frontend/run.sh
@@ -8,4 +8,8 @@ if [[ "$(docker ps | grep hikari-docker:1.0.0 2> /dev/null)" == "" ]]; then
   docker run -it -d -p 9000:9000 -p 35729:35729 -v $1/src/client:/src/client hikari-docker:1.0.0
 fi
 
+socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
+ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+docker run -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix firefox
+
 open http:/0.0.0.0:9000
