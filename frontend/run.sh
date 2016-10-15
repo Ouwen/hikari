d21316a59da3c99928e19d23196a3ca56366668b
Ouwen Huang <oh11@Ouwens-MacBook-Pro.local>
2016-10-14 21:49:59 -0400

    Fixed frontend run script

--- frontend/run.sh
+++ frontend/run.sh
@@ -5,11 +5,8 @@ if [[ "$(docker images -q hikari-docker:1.0.0 2> /dev/null)" == "" ]]; then
 fi
 
 if [[ "$(docker ps | grep hikari-docker:1.0.0 2> /dev/null)" == "" ]]; then
-  docker run -it -d -p 9000:9000 -p 35729:35729 -v $1/src/client:/src/client hikari-docker:1.0.0
+  docker run -it -d \
+  -p 9000:9000 -p 35729:35729 -v $1/src/client:/src/client hikari-docker:1.0.0
 fi
-
-socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
-ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
-docker run -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix firefox
 
 open http:/0.0.0.0:9000
