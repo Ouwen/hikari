e1483fac120864a0eb6db5715417b7c7dddeb446
Ouwen Huang <oh11@Ouwens-MBP.dhe.duke.edu>
2016-11-07 14:32:01 -0500

    Updated nginx config

--- backend/server/routes.js
+++ backend/server/routes.js
@@ -7,8 +7,15 @@
 module.exports = function (app) {
 
   // Insert routes below
-  app.use('/api/users', require('./api/user'));
-  app.use('/auth', require('./auth'));
+  app.use('/v1.0.0/api/users', require('./api/user'));
+  app.use('/v1.0.0/auth', require('./auth'));
+
+  app.route('/v1.0.0').get(function (req, res){
+    return res.json({
+      version: '1.0.0',
+      environment: process.env.NODE_ENV
+    });
+  });
 
   app.route('/').get(function (req, res){
     return res.json({
@@ -16,7 +23,7 @@ module.exports = function (app) {
       environment: process.env.NODE_ENV
     });
   });
-  
+
   // All other routes get a 404
   app.route('/*').get(function (req, res) {
     return res.status(404).json("404 not found.");
