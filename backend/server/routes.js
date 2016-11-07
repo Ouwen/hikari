2d518b52d11358cd7b0eb9f4e3248415028feda3
Ouwen Huang <oh11@Ouwens-MBP.dhe.duke.edu>
2016-11-07 16:33:22 -0500

    Added core prefix to nginx

--- backend/server/routes.js
+++ backend/server/routes.js
@@ -7,10 +7,10 @@
 module.exports = function (app) {
 
   // Insert routes below
-  app.use('/v1.0.0/api/users', require('./api/user'));
-  app.use('/v1.0.0/auth', require('./auth'));
+  app.use('/core/v1.0.0/api/users', require('./api/user'));
+  app.use('/core/v1.0.0/auth', require('./auth'));
 
-  app.route('/v1.0.0').get(function (req, res){
+  app.route('/core/v1.0.0').get(function (req, res){
     return res.json({
       version: '1.0.0',
       environment: process.env.NODE_ENV
