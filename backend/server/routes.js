b9ed35b5158b916a476b9f61cd8614ff4b315593
Ouwen Huang <oh11@Ouwens-MBP.dhe.duke.edu>
2016-11-07 13:56:12 -0500

    Added nginx config

--- backend/server/routes.js
+++ backend/server/routes.js
@@ -10,6 +10,13 @@ module.exports = function (app) {
   app.use('/api/users', require('./api/user'));
   app.use('/auth', require('./auth'));
 
+  app.route('/').get(function (req, res){
+    return res.json({
+      version: '1.0.0',
+      environment: process.env.NODE_ENV
+    });
+  });
+  
   // All other routes get a 404
   app.route('/*').get(function (req, res) {
     return res.status(404).json("404 not found.");
